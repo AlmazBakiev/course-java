90149d519489bc62b68ee3377ad7f40b204fe1b4
Almaz Bakiev <black-diamond-sar@mail.ru>
2022-04-16 11:14:09 +0400

    complete task

--- src/main/java/ru/gb/java/almazbakiev/HomeWork2.java
+++ src/main/java/ru/gb/java/almazbakiev/HomeWork2.java
@@ -4,8 +4,12 @@ public class HomeWork2 {
     public static void main(String[] args) {
         System.out.println(addingTwoNumbers(9, 11));
         positiveOrNegativeNumber(0);
+        System.out.println(isNegative(5));
+        printString(3, "Home work");
+        System.out.println(leapYear(123));
     }
-// First task
+
+    // First task
     public static boolean addingTwoNumbers(int firstNumber, int secondNumber) {
         firstNumber += secondNumber;
         if (firstNumber >= 10 && firstNumber <= 20) {
@@ -14,14 +18,43 @@ public class HomeWork2 {
             return false;
         }
     }
-// Second task
+
+    // Second task
     public static void positiveOrNegativeNumber(int number) {
         if (number < 0) {
             System.out.println("Число отрицательное!");
         } else {
             System.out.println("Число положительное!");
         }
-
     }
 
+    // Third task
+    public static boolean isNegative(int number) {
+        if (number < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Fourth task
+    public static void printString(int iterationCounter, String text) {
+        while (iterationCounter != 0 && iterationCounter > 0) {
+            System.out.println(text);
+            iterationCounter--;
+        }
+    }
+
+    // Fifth task
+    public static boolean leapYear(int year) {
+        if ((year %= 400) == 0) {
+            return true;
+        } else if ((year %= 100) == 0) {
+            return false;
+        } else if ((year %= 4) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
