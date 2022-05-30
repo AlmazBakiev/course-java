b555f461aedcce5afb592dabf58ff1b80e75e240
Almaz Bakiev <black-diamond-sar@mail.ru>
2022-05-30 12:30:19 +0400

    java level 2 HM1 completed

--- src/main/java/ru/gb/javaLvl2/almazbakiev/Obstacles.java
+++ src/main/java/ru/gb/javaLvl2/almazbakiev/Obstacles.java
@@ -1,11 +1,24 @@
 package ru.gb.javaLvl2.almazbakiev;
 
+
 public abstract class Obstacles {
     private String name;
 
     public String getName() {
         return name;
     }
+
+//    public void runAndJump(Object team, Object o) {
+//        if (o.getClass().getName().equals("Wall")) {
+//            if (team.getClass().) {
+//                break;
+//            }
+//        } else {
+//            if (athleticSkills.run(o.getLengthOrHeightObstacle())) {
+//                break;
+//            }
+//        }
+//    }
 
     public abstract int getLengthOrHeightObstacle();
 }
