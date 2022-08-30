7e15c7ec04d01fe951a71ca639d9d8ca6c2a29a1
Almaz Bakiev <black-diamond-sar@mail.ru>
2022-08-30 20:00:14 +0400

    homework-5 completed

---
+++ src/main/java/ru/gb/javalvl3/almazbakiev/MainClass.java
@@ -0,0 +1,32 @@
+package ru.gb.javalvl3.almazbakiev;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+
+    public static final int CARS_COUNT = 4;
+    public static final CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+    public static final CountDownLatch raceStarted = new CountDownLatch(CARS_COUNT);
+    public static final CountDownLatch raceOver = new CountDownLatch(CARS_COUNT);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        try {
+            raceStarted.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            raceOver.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/gb/javalvl3/almazbakiev/Race.java
@@ -0,0 +1,18 @@
+package ru.gb.javalvl3.almazbakiev;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+
+    private ArrayList<Stage> stages;
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+
+}
