2c3bf510a54b6f1ee14aff0cd8c72e731b0468de
Almaz Bakiev <black-diamond-sar@mail.ru>
2022-09-06 17:18:16 +0400

    HM6 JUnit completed

---
+++ src/main/java/ru/gb/javalvl3/almazbakiev/EmptyArrayException.java
@@ -0,0 +1,7 @@
+package ru.gb.javalvl3.almazbakiev;
+
+public class EmptyArrayException extends RuntimeException {
+    public EmptyArrayException(String message) {
+        super(message);
+    }
+}
